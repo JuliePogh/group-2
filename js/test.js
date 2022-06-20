a6b07c113f5f7d638d741fd41b8d3fcc2832f341
Juli Poghosyan <julipoghosyan@Julis-MacBook-Pro.local>
2022-06-19 23:41:15 -0400

    My first commit

---
+++ js/test.js
@@ -0,0 +1,67 @@
+let i = 0;
+while(i <= 10){
+	console.log(i);
+	i++;
+}
+let z = 20;
+while(z >= 10){
+	console.log(z);
+	z--;
+}
+
+
+let price = 55;
+while(price >= 40){
+	console.log(price);
+	price--;
+}
+
+let age = 25;
+while(age <= 60){
+	console.log(age);
+	age++;
+}
+
+let fruits = ["banana","apple","strawberry"];
+ let h = 0;
+while(fruits.length > h){
+	console.log(fruits[h]);
+	h++;
+}
+
+
+for( let i = 30; i <= 35; i++){
+	console.log(i);
+}
+
+let num = 5;
+for(let sum = 11; sum >= num; sum--) {
+	console.log(sum);
+  }
+
+  let dayTasks = "Infinty";
+  for(let a = 0; a <= dayTasks.length; a++){
+	  console.log(a);
+  }
+
+  let command = ["Start","Break","End"];
+  for(let i = 0; i < command.length;i++){
+	  console.log(command[i]);
+  }
+
+  let myNameIs = "Juli";
+  for(let b = 0; b <= myNameIs.length; b++){
+	  console.log(b);
+  }
+  
+  
+ let me = 32;
+ let mySister = 20;
+ do{
+	 console.log(me);
+	 me--;
+ } while(me >= mySister);
+  
+
+
+  
